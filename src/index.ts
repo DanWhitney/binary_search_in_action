a4b7c222ce6fed8fe155cabe26f0bb78f813f8d4
DanWhitney <dwcoding101@gmail.com>
2020-08-26 08:10:46 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
 console.log("test")
+
+let findElement: <T> (sortedArr: T[], element: T, comparatorFn:(item: T, element: T) => boolean ) => number = function (sortedArr, element) {
+  let startIndex = 0
+  let endIndex = sortedArr.length -1
+  while (startIndex <= endIndex) {
+    const middleIndex = startIndex + Math.floor((endIndex -  startIndex) / 2)
+
+    if (element === sortedArr[middleIndex]) {
+      return middleIndex;
+    }
+
+    if (sortedArr[middleIndex] < element) {
+      startIndex = middleIndex + 1
+    } else {
+      endIndex = middleIndex - 1
+    }
+  }
+
+
+  return -1
+}
+
+const arr = [1, 5, 9, 13, 99, 100];
+
+console.log(findElement(arr, 99,( item, element) => item===element))
